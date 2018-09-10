1d08040bdc1357baef4eb7bdca6d8c19d22cb3c5
willy <willyboy129@gmail.com>
2018-09-10 15:20:35 +0100

    new project

---
+++ src/main/java/clientgestionstock/clientgestionstock/App.java
@@ -0,0 +1,13 @@
+package clientgestionstock.clientgestionstock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
